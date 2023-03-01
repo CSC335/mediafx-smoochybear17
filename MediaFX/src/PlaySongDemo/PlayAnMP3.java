a1bfbd6556fe40664f15cd689e57f541a4404b62
Neftali Nunez <neftalinunez@arizona.edu>
2023-03-01 01:49:32 -0700

    Made small changes to PlayOneMP3.java

--- MediaFX/src/PlaySongDemo/PlayAnMP3.java
+++ MediaFX/src/PlaySongDemo/PlayAnMP3.java
@@ -28,7 +28,8 @@ public class PlayAnMP3 extends Application {
   @Override
   public void start(Stage stage) throws Exception {
     BorderPane pane = new BorderPane();
-    String path = "songfiles/Capture.mp3";
+    // I, Neftali Nunez, changed the song to be played before pushing to GitHub
+    String path = "songfiles/DanseMacabreViolinHook.mp3";
     pane.setCenter( new Label(path));
     playASong(path);
     // Put the pane in a sized Scene and show the GUI
